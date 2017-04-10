9bfbb2b5e31b5977ae568d2f9600a52d659087c8
siromivel <siromivel@gmail.com>
2017-04-10 08:21:55 -0400

    Swap default to Lansing

--- src/City.js
+++ src/City.js
@@ -4,8 +4,8 @@ import Cities from './Cities';
 // city that the app references
 
 let getCity = function getCity(city) {
-  return Cities.find(c => c.url === city) || Cities.find(c => c.url === 'annarbor');
+  return Cities.find(c => c.url === city) || Cities.find(c => c.url === 'lansing');
 };
 
-let current = location && location.pathname.length > 1 ? location.pathname.match(/\/(.[^/]+)/)[1] : 'annarbor';
-export default getCity(current) ? getCity(current) : getCity("annarbor");
+let current = location && location.pathname.length > 1 ? location.pathname.match(/\/(.[^/]+)/)[1] : 'lansing';
+export default getCity(current) ? getCity(current) : getCity("lansing");
